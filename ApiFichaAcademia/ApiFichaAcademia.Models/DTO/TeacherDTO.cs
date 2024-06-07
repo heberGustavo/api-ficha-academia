7fd0b7012eaee53a5df2df1cf343e9558453422e
heberGustavo <heber.gbarbosa@hotmail.com>
2024-06-07 08:17:53 -0300

    Updating return of the data to show the name of the Teacher Period

--- ApiFichaAcademia/ApiFichaAcademia.Models/DTO/TeacherDTO.cs
+++ ApiFichaAcademia/ApiFichaAcademia.Models/DTO/TeacherDTO.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ApiFichaAcademia.Common;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ApiFichaAcademia.Models.DTO
@@ -19,5 +21,23 @@ namespace ApiFichaAcademia.Models.DTO
 
 		[Required, Range(1, 3, ErrorMessage = "It's necessary send the Period. Morning, Afternoon or Night")]
 		public int Period { get; set; }
-	}
+
+        public string PeriodDescription {
+			get 
+			{
+				if (Period == (int)EnumPeriod.Morning) _periodDescription = "Morning";
+				else if (Period == (int)EnumPeriod.Afternoon) _periodDescription = "Afternoon";
+				else if (Period == (int)EnumPeriod.Night) _periodDescription = "Night";
+
+				return _periodDescription;
+			}
+			set 
+			{
+				_periodDescription = value;
+			} 
+		}
+
+		[JsonIgnore]
+		public string _periodDescription { get; set; } = string.Empty;
+    }
 }
